f16db35e516d0d3f696a03f6a4f7b85f7b60a281
Bill <207559339@qq.com>
2019-11-10 15:30:49 +0800

    homework

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import cityData from 'all-the-cities'
-import distanceFrom from 'distance-from'
-import { Button } from 'semantic-ui-react'
+import React from 'react' // import react
+import cityData from 'all-the-cities' // import all-the-cities
+import distanceFrom from 'distance-from' // import distance-from
+import { Button } from 'semantic-ui-react' // import semantic-ui-react
 
-const currentCity = cityData.find(c => c.name === "Chengdu")
-const closeCities = cityData.filter(c => distanceFrom(currentCity.loc.coordinates).to(c.loc.coordinates) < 100)
+const currentCity = cityData.find(c => c.name === "Chengdu") // find Chengdu
+const closeCities = cityData.filter(c => distanceFrom(currentCity.loc.coordinates).to(c.loc.coordinates) < 100) //find the city within 100 km from chengdu
 
-class App extends React.Component{
-    render(){
-        return <div>
-            {closeCities.map(c => <div><Button>{c.name}</Button></div>)}
+class App extends React.Component{ // write a class named App
+    render(){ // render is required in a React class
+        return <div> 
+            {closeCities.map(c => <div><Button>{c.name}</Button></div>)} 
         </div>
-    }
+    } // return should be in render, map out each city name into a button
 }
 
-export default App
+export default App // export App
